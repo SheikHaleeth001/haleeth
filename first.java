fc75c7e12d1ebbca3e3233096393e569a315c508
Sheik <55941177+SheikHaleeth001@users.noreply.github.com>
2023-04-04 16:36:46 +0530

    changed values

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ class first
 {  
     public static void main(String args[])   
 {  
-int n1 = 67, n2 = 5, sum;  
+int n1 = 17, n2 = 10, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
