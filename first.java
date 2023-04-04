d796a05c206ef91432689231cca6a6f73c309d79
Sheik <55941177+SheikHaleeth001@users.noreply.github.com>
2023-04-04 16:40:20 +0530

    changed variable name

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ class first
 {  
     public static void main(String args[])   
 {  
-int n1 = 17, n2 = 10, sum;  
+int a = 17, b = 10, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
