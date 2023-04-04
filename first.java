2ba8d7263dde71d10b709739114f0c1086259f26
haleeth <haleethtmmk@gmail.com>
2023-04-04 16:57:50 +0530

    change values a

--- first.java
+++ first.java
@@ -2,8 +2,8 @@ class first
 {  
     public static void main(String args[])   
 {  
-int a = 17, b = 10, sum;  
-sum = n1 + n2;  
+int a = 7, b = 10, sum;  
+sum = a + b;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  
